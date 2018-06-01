afdc640269a3b80e3a2a65f60b2155df3367abcc
Roytsai27 <38278579+Roytsai27@users.noreply.github.com>
2018-06-01 14:59:55 +0800

    add more detail

--- new.py
+++ new.py
@@ -1,62 +1,58 @@
 import pandas as pd
 from sklearn.pipeline import Pipeline
-import nltk
 from nltk.stem.snowball import SnowballStemmer
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 from nltk import word_tokenize  
+from sklearn.feature_extraction.text import TfidfVectorizer ,TfidfTransformer,CountVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.svm import SVR
+from sklearn.model_selection import GridSearchCV, KFold
+
+#Load data
+
+df1 = pd.read_csv("training_data.csv")
+df2 = pd.read_csv("test_data.csv")
+
+#prepocessing 
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+#build Tokenizer and Stemmer to handle the text data
+
 class LemmaTokenizer(object):
 	def __init__(self):
 		self.wnl = WordNetLemmatizer()
 	def __call__(self, doc):
 		return [self.wnl.lemmatize(t) for t in word_tokenize(doc)]
-df1 = pd.read_csv("training_data.csv")
-df2 = pd.read_csv("test_data.csv")
 
-text_train = df1["text"]
-text_test = df2["text"]
-y_train = df1["stars"]
-
-from sklearn.feature_extraction.text import TfidfVectorizer ,TfidfTransformer,CountVectorizer
-from sklearn.decomposition import TruncatedSVD
-
-nltk.download('stopwords')
-nltk.download('wordnet')
 stemmer = SnowballStemmer("english", ignore_stopwords=True)
-# stemmer = PorterStemmer()
+
+#In order to trasform text data into vectors
+#We combine the Stemmer with sklearn-Countvectorizer
 class StemmedCountVectorizer(CountVectorizer):
     def build_analyzer(self):
         analyzer = super(StemmedCountVectorizer, self).build_analyzer()
         return lambda doc: ([stemmer.stem(w) for w in analyzer(doc)])
 
+#there's about 24000 words we want to reduce the dimension
+# And Build a pipeline to make our code more clear
+stemed_vec = StemmedCountVectorizer(min_df= 2,ngram_range=(1,2),tokenizer = LemmaTokenizer())
 
-svd = TruncatedSVD(n_components = 1000)
-vectorizer = TfidfVectorizer()
-vec = StemmedCountVectorizer(min_df= 2,ngram_range=(1,2),tokenizer = LemmaTokenizer())
-# vectorizer.fit(text_train)
+clf = Pipeline([("vec",stemed_vec),("tf-idf",TfidfTransformer()),
+	            ("SVD",TruncatedSVD(n_components=3000)),("svr",SVR(C=10,gamma=1,verbose=1))])
+#Use GridSearch to tune our parameters
 
-# X_train = vectorizer.transform(text_train)
-# X_test = vectorizer.transform(text_test)
-
-# X_SVD_train = svd.fit_transform(X_train)
-# X_SVD_test = svd.transform(X_test)
-from sklearn.svm import SVR
-from sklearn.model_selection import GridSearchCV, KFold
-from sklearn.linear_model import SGDRegressor
-
-
-clf = Pipeline([("vec",vec),("tf-idf",TfidfTransformer()),
-	            ("SVD",TruncatedSVD(n_components=3000)),("svc",SVR(C=10,gamma=1,verbose=1))])
-
-#param_grid = {'C': [0.001, 0.01, 0.1, 1, 10], 'gamma': [0.001, 0.01, 0.1, 1]}
-# param_grid = {"SVD__n_components":[1000,2000,3000]}
-# cv = KFold(shuffle=True)
-# clf = GridSearchCV(pipe, param_grid=param_grid, cv=cv, verbose=3)
-#grid.fit(X_train, y_train)
-#grid.predict(X_test)
+param_grid = {'svr__C': [0.001, 0.01, 0.1, 1, 10], 'svr__gamma': [0.001, 0.01, 0.1, 1]
+              'SVD__n_components':[1000,2000,3000,4000],'vec_ngram_range':[(1,1),(1,2),(1,3)],
+              'tfidf__use_idf': (True, False)}
+cv = KFold(shuffle=True)
+grid = GridSearchCV(pipe, param_grid=param_grid, cv=cv, verbose=3)
+grid.fit(X_train, y_train)
+grid.predict(X_test)
 #print(grid.best_score_) # 0.4652
 #print(grid.best_params_) # {'C': 10, 'gamma': 1}
-# clf = SVR(C=10, gamma=1)
 
 clf.fit(text_train, y_train)
 y_test = clf.predict(text_test)
